a9b6d8dfe1363eec9e7e32e5f1404c48abc35f8b
Pearl Akpan <pearlakpan115@gmail.com>
2022-10-17 14:21:39 +0100

    chore: write pure filter component & fnality

--- reminders-app/src/components/FilterSelect.js
+++ reminders-app/src/components/FilterSelect.js
@@ -1,15 +1,30 @@
+import PropTypes from 'prop-types';
+
 function FilterSelect(props) {
+  function handleChange(e){
+    props.setSelectedFilter(e.target.value);
+  }
+
   return (
     <label htmlFor="filterReminders">
       Show tasks due:
-      {/* <select id="filterReminders" value="2day">
+      <select id="filterReminders" value={props.selectedFilter} onChange={handleChange}>
         <option value="2day">within 2 Days</option>
         <option value="1week">within 1 Week</option>
         <option value="30days">within 30 Days</option>
-        <option value="all">any time</option>
-      </select> */}
+        <option value="all">all time</option>
+      </select>
     </label>
   );
 }
+
+FilterSelect.propTypes = {
+  selectedFilter: PropTypes.string,
+  setSelectedFilter: PropTypes.func
+};
+
+FilterSelect.defaultProps = {
+  selectedFilter: "all"
+};
 
 export default FilterSelect;
