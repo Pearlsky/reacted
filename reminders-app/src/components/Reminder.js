4a4904dd83b719706819256ad41177d08a6a543e
Pearl Akpan <pearlakpan115@gmail.com>
2022-10-17 14:23:51 +0100

    refactor: write pure reminder component

--- reminders-app/src/components/Reminder.js
+++ reminders-app/src/components/Reminder.js
@@ -1,11 +1,45 @@
+import PropTypes from "prop-types";
+
 function Reminder(props) {
+  function handleChange() {
+    props.setIsComplete(!props.isComplete, props.id);
+  }
+
   return (
-    <div>
+    <div className="item">
       item: {props.reminderText}
-      due date: {props.dueDate}
-      Completed?: {String(props.isComplete)}
+      <span
+        className="due-date"
+      >
+        due date: {props.dueDate}
+      </span>
+      <span
+        className="is-complete"
+      >
+        Completed?:{" "}
+        <input
+          type="checkbox"
+          checked={props.isComplete}
+          onChange={handleChange}
+        />
+      </span>
     </div>
   );
 }
+
+Reminder.propTypes = {
+  reminderText: PropTypes.string,
+  dueDate: PropTypes.string,
+  isComplete: PropTypes.bool,
+};
+
+const date = new Date();
+const formattedDate = date.toISOString().substring(0, 10);
+
+Reminder.defaultProps = {
+  reminderText: "No Reminder Set",
+  dueDate: formattedDate,
+  isComplete: false,
+};
 
 export default Reminder;
