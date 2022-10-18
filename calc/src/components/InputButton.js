4c7a67c8b8e1d854f22fbc16cc4b6371e4a1f42e
Pearl Akpan <pearlakpan115@gmail.com>
2022-10-18 13:35:34 +0100

    chore: write operand & operator components and event handling

--- calc/src/components/InputButton.js
+++ calc/src/components/InputButton.js
@@ -1,5 +1,36 @@
-function InputButton({value, onClick}) {
-    return <input type="button" value={value} onClick={onClick}/>
+function Operator({
+  value,
+  memoryOperand,
+  setMemoryOperand,
+  currentOperand,
+  setCurrentOperand,
+  setScreenValue,
+  operator,
+  setOperator,
+  func,
+}) {
+  const handleOperClick = () => {
+    const operFunc =
+      operator === "" 
+      ? setOperator((prev) => (prev = func)) 
+      : operator;
+
+    const memoryValue =
+      memoryOperand === ""
+        ? currentOperand
+        : operFunc(Number(memoryOperand), Number(currentOperand));
+
+    setMemoryOperand((prev) => (prev = memoryValue));
+    setScreenValue((prev) => (prev = memoryValue));
+    setCurrentOperand((prev) => (prev = ""));
+    setOperator((prev) => (prev = func));
+  };
+
+  return <input type="button" value={value} onClick={handleOperClick} />;
 }
 
-export default InputButton;
+function Operand({ value, onClick }) {
+  return <input type="button" value={value} onClick={onClick} />;
+}
+
+export { Operator, Operand };
