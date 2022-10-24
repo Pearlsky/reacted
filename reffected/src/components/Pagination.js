94b045f04750108b3d604793a8118c8cc2fc0387
Pearl Akpan <pearlakpan115@gmail.com>
2022-10-24 13:15:31 +0100

    feat: set up pagination for users page

---
+++ reffected/src/components/Pagination.js
@@ -0,0 +1,48 @@
+function Pagination({ users, usersPerPage, currentPage, setCurrentPage }) {
+  const nPages = Math.ceil(users.length / usersPerPage);
+  const pageNumbers = [];
+
+  for (let i = 1; i <= nPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const pageSelectHandler = (num) => {
+    setCurrentPage(num);
+  };
+
+  const prevPageHandler = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1 )
+    }
+  };
+
+  const nextPageHandler = () => {
+    if (currentPage < nPages) {
+      setCurrentPage((prev) => prev + 1 )
+    }
+  };
+
+  return (
+    <>
+      <section className="page-controls">
+        <nav className="page-nav">
+          <ul>
+            <li key="page-prev">
+              <button onClick={() => prevPageHandler()}>Prev</button>
+            </li>
+            {pageNumbers.map((pageNumber, index) => (
+              <li key={`page-${index}`}>
+                <button onClick={() => pageSelectHandler(pageNumber)}>{pageNumber}</button>
+              </li>
+            ))}
+            <li key="page-next">
+              <button onClick={() => nextPageHandler()}>Next</button>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </>
+  );
+}
+
+export default Pagination;
