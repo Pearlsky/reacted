ec8580f9d6770cf3e7249c4a7ba8703113ca70d6
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-07 15:21:14 +0100

    chore: set error counter trigger

--- reffected/src/components/Portals/Portals.js
+++ reffected/src/components/Portals/Portals.js
@@ -1,5 +1,42 @@
+import { useState, useEffect } from "react";
+
 function Portals() {
-    return <main className="main"></main>;
-  }
-  
-  export default Portals;
+  const [, setDateInput] = useState("");
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count > 3) {
+      throw new Error();
+    }
+  }, [count]);
+  return (
+    <main className="main">
+      <form>
+        <label htmlFor="portalDateInput">
+          <input
+            id="portalDateInput"
+            onChange={(e) => {
+              setDateInput(e.target.value);
+            }}
+            type="text"
+            placeholder="Type a portal day to set"
+          />
+        </label>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setCount((prev) => prev + 1);
+          }}
+        >
+          Set Date
+        </button>
+      </form>
+      <div>
+        <p>You're allowed only three date slots. Try not to exceed it</p>
+        <p>You've set the date {count} times</p>
+      </div>
+    </main>
+  );
+}
+
+export default Portals;
