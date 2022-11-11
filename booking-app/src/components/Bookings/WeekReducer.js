8e97f6682e53fbc77e6bedbe7fa0e6a5b211f76e
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-11 14:29:53 +0100

    chore: write reducer to handle state logic in week picker

---
+++ booking-app/src/components/Bookings/WeekReducer.js
@@ -0,0 +1,16 @@
+import { getWeek } from "../../utils/date-wrangler";
+
+export default function WeekReducer (state, action) {
+    switch(action.type) {
+        case "PREV_WEEK" : 
+            return getWeek(state.date, -7);
+        case "NEXT_WEEK" : 
+            return getWeek(state.date, 7);
+        case "TODAY" : 
+            return getWeek(new Date());
+        case "SET_DAY" : 
+            return getWeek(new Date(action.payload));
+        default : 
+            return `No know action as ${action.type}`;
+    }
+}
