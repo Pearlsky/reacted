3b13024394070796e3e72503d8e1fb8ea04f10d7
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-14 16:17:22 +0100

    refactor: improve reducer to include data fetching loading and error states

--- booking-app/src/components/Bookables/BookablesReducer.js
+++ booking-app/src/components/Bookables/BookablesReducer.js
@@ -12,8 +12,30 @@ export default function BookablesReducer(state, action) {
       ).length;
       return { ...state, bookableIndex: (state.bookableIndex + 1) % count };
 
-    case "TOGGLE_HAS_DETAILS": 
-        return {...state, hasDetails: !state.hasDetails}    
+    case "TOGGLE_HAS_DETAILS":
+      return { ...state, hasDetails: !state.hasDetails };
+
+    case "FETCH_BOOKABLES_REQUEST":
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+        bookables: [],
+      };
+
+    case "FETCH_BOOKABLES_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        bookables: action.payload,
+      };
+
+    case "FETCH_BOOKABLES_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
 
     default:
       throw new Error(`Unknown action type: ${action.type}`);
