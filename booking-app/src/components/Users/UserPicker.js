fbbd67c9b92586e75dd910a5898133a3e70b05ce
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-14 15:23:20 +0100

    refactor: write user picker component to fetch users data and add loading states

--- booking-app/src/components/Users/UserPicker.js
+++ booking-app/src/components/Users/UserPicker.js
@@ -1,7 +1,18 @@
-import data from "../../static.json";
+import { useState, useEffect } from "react";
 
 export default function UserPicker() {
-  const { users } = data;
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+    .then(response => response.json())
+    .then((data) => {setUsers(data)})
+    .catch((err) => {throw new Error(`This ${err.type} error just occured: ${err.message}`)});
+  }, []);
+
+  if(users === null) {
+    return <h1>Loading...</h1>
+  }
 
   return (
     <select>
