3ec39d4bc823b8ba4a2afc2237d5e016c2ea1122
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-21 17:22:18 +0100

    refactor: modify bookables state to manage local state w useState not reducers

--- booking-app/src/components/Bookables/BookablesList.js
+++ booking-app/src/components/Bookables/BookablesList.js
@@ -1,49 +1,61 @@
-import { useEffect } from "react";
-import { Fragment, useRef } from "react";
+import { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { getData } from "../../utils/api";
 
-export default function BookablesList({ state, dispatch }) {
-  const nextButtonRef = useRef();
+export default function BookablesList({ bookable, setBookable }) {
+  const [bookables, setBookables] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
-  const { bookables, bookableIndex, group } = state;
-  const { isLoading, error } = state;
-
-  useEffect(() => {
-    dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
-
-    getData("http://localhost:3001/bookables")
-      .then((bookables) =>
-        dispatch({ type: "FETCH_BOOKABLES_SUCCESS", payload: bookables })
-      )
-      .catch((error) =>
-        dispatch({ type: "FETCH_BOOKABLES_ERROR", payload: error })
-      );
-  }, [dispatch]);
-
+  const group = bookable?.group;
   const groups = [...new Set(bookables && bookables.map((b) => b.group))];
   const bookablesInGroup =
     bookables && bookables.filter((bookable) => bookable.group === group);
 
-  function changeBookableIndex(index) {
-    dispatch({
-      type: "SET_BOOKABLE",
-      payload: index,
-    });
-    nextButtonRef.current.focus();
+  // const { bookables, bookableIndex, group } = state;
+  // const { isLoading, error } = state;
+
+  // useEffect(() => {
+  //   dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
+
+  //   getData("http://localhost:3001/bookables")
+  //     .then((bookables) =>
+  //       dispatch({ type: "FETCH_BOOKABLES_SUCCESS", payload: bookables })
+  //     )
+  //     .catch((error) =>
+  //       dispatch({ type: "FETCH_BOOKABLES_ERROR", payload: error })
+  //     );
+  // }, [dispatch]);
+
+  useEffect(() => {
+    getData("http://localhost:3001/bookables")
+      .then((bookables) => {
+        setBookable(bookables[0]);
+        setBookables(bookables);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [setBookable]);
+
+  function changeBookable(selectedBookable) {
+    setBookable(selectedBookable);
   }
 
   function changeGroup(e) {
-    dispatch({
-      type: "SET_GROUP",
-      payload: e.target.value,
-    });
+    const bookablesInSelectedGroup = bookables.filter(
+      (b) => b.group === e.target.value
+    );
+    setBookable(bookablesInSelectedGroup[0]);
   }
 
   function nextBookable() {
-    dispatch({
-      type: "NEXT_BOOKABLE",
-    });
+    const i = bookablesInGroup.indexOf(bookable);
+    const nextIndex = (i + 1) % bookablesInGroup.length;
+    const nextBookable = bookablesInGroup[nextIndex];
+    setBookable(nextBookable);
   }
 
   if (isLoading) {
@@ -55,47 +67,40 @@ export default function BookablesList({ state, dispatch }) {
   }
 
   return (
-    <Fragment>
-      <div>
-        <select
-          value={group}
-          onChange={(e) => {
-            changeGroup(e);
-          }}
-        >
-          {groups.map((g) => (
-            <option value={g} key={g}>
-              {g}
-            </option>
-          ))}
-        </select>
-        <ul className="bookables items-list-nav">
-          {bookablesInGroup.map((b, i) => (
-            <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-              <button
-                className="btn"
-                onClick={() => {
-                  changeBookableIndex(i);
-                }}
-              >
-                {b.title}
-              </button>
-            </li>
-          ))}
-        </ul>
+    <div>
+      <select
+        value={group}
+        onChange={(e) => {
+          changeGroup(e);
+        }}
+      >
+        {groups.map((g) => (
+          <option value={g} key={g}>
+            {g}
+          </option>
+        ))}
+      </select>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={b.id === bookable.id ? "selected" : null}>
+            <button
+              className="btn"
+              onClick={(e) => {
+                changeBookable(b);
+              }}
+            >
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
 
-        <p>
-          <button
-            className="btn"
-            onClick={nextBookable}
-            ref={nextButtonRef}
-            autoFocus
-          >
-            <FaArrowRight />
-            <span>Next</span>
-          </button>
-        </p>
-      </div>
-    </Fragment>
+      <p>
+        <button className="btn" onClick={nextBookable} autoFocus>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 }
