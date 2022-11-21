5c73d2ab16c2201f63ebe68d48ebe42013e648eb
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-21 17:21:00 +0100

    refactor: lift shared bookable state

--- booking-app/src/components/Bookables/BookablesView.js
+++ booking-app/src/components/Bookables/BookablesView.js
@@ -1,27 +1,31 @@
-import { useReducer } from "react";
+// import { useReducer } from "react";
+import { useState } from "react";
 import BookablesList from "./BookablesList";
 import BookablesDetails from "./BookablesDetails";
-import BookablesReducer from "./BookablesReducer";
+// import BookablesReducer from "./BookablesReducer";
 
-const initialState = {
-  bookableIndex: 0,
-  group: "Kit",
-  isLoading: true,
-  bookables: [],
-  error: false,
-};
+// const initialState = {
+//   bookableIndex: 0,
+//   group: "Kit",
+//   isLoading: true,
+//   bookables: [],
+//   error: false,
+// };
 
 export default function BookablesView() {
-  const [state, dispatch] = useReducer(BookablesReducer, initialState);
+    // lift bookable state being the only state the two components share
+    const [bookable, setBookable] = useState();
 
-  const bookablesInGroup =
-    state.bookables &&
-    state.bookables.filter((bookable) => bookable.group === state.group);
-  const bookable = bookablesInGroup[state.bookableIndex];
+  // const [state, dispatch] = useReducer(BookablesReducer, initialState);
+
+  // const bookablesInGroup =
+  //   state.bookables &&
+  //   state.bookables.filter((bookable) => bookable.group === state.group);
+  // const bookable = bookablesInGroup[state.bookableIndex];
 
   return (
     <>
-      <BookablesList state={state} dispatch={dispatch} />
+      <BookablesList bookable={bookable} setBookable={setBookable} />
       <BookablesDetails bookable={bookable} />
     </>
   );
