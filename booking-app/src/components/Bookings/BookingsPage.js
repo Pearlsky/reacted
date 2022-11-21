c8e66b641d8dcd983667dae38ca8bccf24c44711
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-21 17:27:13 +0100

    refactor: modify bookings page to show two views

--- booking-app/src/components/Bookings/BookingsPage.js
+++ booking-app/src/components/Bookings/BookingsPage.js
@@ -1,10 +1,14 @@
-import WeekPicker from "./WeekPicker";
+import { useState } from "react";
+import BookablesList from "../Bookables/BookablesList";
+import Bookings from "./Bookings";
 
 export default function BookingsPage() {
+  const [bookable, setBookable] = useState(null);
+
   return (
     <main className="bookings-page">
-      <p>Bookings!</p>
-      <WeekPicker date={new Date()}/>
+      <BookablesList bookable={bookable} setBookable={setBookable} />
+      <Bookings bookable={bookable}/>
     </main>
   );
 }
