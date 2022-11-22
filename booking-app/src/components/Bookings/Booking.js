fc16c93f1ab4a30938efbbfd5c080c4da19b9969
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-22 19:44:41 +0100

    feat: add booking details component to show and edit booking details based on booker id

---
+++ booking-app/src/components/Bookings/Booking.js
@@ -0,0 +1,26 @@
+export default function Booking({ booking, bookable }) {
+  const { title, date, session, notes } = booking;
+
+  return (
+    <div className="booking-details-fields">
+      <label>Title</label>
+      <p>{title}</p>
+
+      <label>Bookable</label>
+      <p>{bookable.title}</p>
+
+      <label>Date</label>
+      <p>{new Date(date).toDateString()}</p>
+
+      <label>Session</label>
+      <p>{session}</p>
+
+      {notes && (
+        <>
+          <label>Notes</label>
+          <p>{notes}</p>
+        </>
+      )}
+    </div>
+  );
+}
