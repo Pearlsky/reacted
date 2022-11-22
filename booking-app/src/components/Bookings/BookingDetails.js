2ff00554aec80ad469cb367c96e365c8e5defeae
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-22 19:47:18 +0100

    feat: add booking details component to show and edit booking details based on booker id

--- booking-app/src/components/Bookings/BookingDetails.js
+++ booking-app/src/components/Bookings/BookingDetails.js
@@ -1,7 +1,32 @@
-export default function BookingsDetails({ booking, bookables }) {
+import { useContext } from "react";
+import { FaEdit } from "react-icons/fa";
+
+import Booking from "./Booking";
+import UserContext from "../Users/UserContext";
+
+export default function BookingsDetails({ booking, bookable }) {
+  const user = useContext(UserContext);
+  const isBooker = booking && user && booking.bookerId === user.id;
+
   return (
-    <div className="booking-details placeholder">
-      <h3>Booking Details</h3>
+    <div className="booking-details">
+      <h2>
+        Booking Details{" "}
+        {isBooker && (
+          <span className="controls">
+            <button className="btn">
+              <FaEdit />
+            </button>
+          </span>
+        )}
+      </h2>
+      {booking ? (
+        <Booking bookable={bookable} booking={booking} />
+      ) : (
+        <div className="booking-details-fields">
+          <p>Select a booking or booking slot</p>
+        </div>
+      )}
     </div>
   );
 }
