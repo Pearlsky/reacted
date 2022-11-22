df1447170e55a2c5be7dbb7a26ab12af4c7ae029
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-22 19:48:06 +0100

    refactor: modify user picker component to control user context value

--- booking-app/src/components/Users/UserPicker.js
+++ booking-app/src/components/Users/UserPicker.js
@@ -1,24 +1,37 @@
 import { useState, useEffect } from "react";
+import Spinner from "../UI/Spinner";
 
-export default function UserPicker() {
+export default function UserPicker({ user, setUser }) {
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3001/users")
-    .then(response => response.json())
-    .then((data) => {setUsers(data)})
-    .catch((err) => {throw new Error(`This ${err.type} error just occured: ${err.message}`)});
-  }, []);
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setUser(data[0]);
+      })
+      .catch((err) => {
+        throw new Error(`This ${err.type} error just occured: ${err.message}`);
+      });
+  }, [setUser]);
 
-  if(users === null) {
-    return <h1>Loading...</h1>
+  if (users === null) {
+    return <Spinner/>;
+  }
+
+  function handleSelect(e) {
+    // because the radix representing the base does not always default to 10
+    const selectedID = parseInt(e.target.value, 10);
+    const selectedUser = users.find((u) => u.id === selectedID);
+    setUser(selectedUser);
   }
 
   return (
-    <select>
-      {users.map((user, i) => 
-        <option key={user.id}>{user.name}</option>
-      )}
+    <select className="user-picker" onChange={handleSelect} value={user?.id}>
+      {users.map((u, i) => (
+        <option key={u.id}>{u.name}</option>
+      ))}
     </select>
   );
 }
