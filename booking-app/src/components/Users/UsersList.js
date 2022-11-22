d5a7c17531f9c57a6cd87bea588e365ce936fb89
Pearl Akpan <pearlakpan115@gmail.com>
2022-11-22 19:49:34 +0100

    refactor: modify users list component to use user context

--- booking-app/src/components/Users/UsersList.js
+++ booking-app/src/components/Users/UsersList.js
@@ -1,58 +1,35 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../UI/Spinner";
 
-export default function UsersList() {
+export default function UsersList({ user, setUser }) {
   const [users, setUsers] = useState(null);
-  const [userIndex, setUserIndex] = useState(0);
+
   useEffect(() => {
     fetch("http://localhost:3001/users")
-    .then(response => response.json())
-    .then(data => {setUsers(data)})
-    .catch((err) => {throw new Error(`An ${err.type} just occured: ${err.message}`)})
-  }, []);
-
-  const userSelectHandler = (index) => {
-    setUserIndex(index);
-  };
-  const user = users && users[userIndex];
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        throw new Error(`An ${err.type} just occured: ${err.message}`);
+      });
+  }, [setUser]);
 
   if (users === null) {
-    return <h1>Loading...</h1>
+    return <p><Spinner/> Loading users...</p>;
   }
 
   return (
-    <Fragment>
-      <div>
-        <ul className="users items-list-nav">
-          {users.map((b, i) => (
-            <li key={b.id} className={i === userIndex ? "selected" : null}>
-              <button
-                className="btn"
-                onClick={() => {
-                  userSelectHandler(i);
-                }}
-              >
-                {b.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {user && (
-        <div className="user-details">
-          <div className="item">
-            <div className="item-header">
-              <h2>{user.name}</h2>
-            </div>
-            <div className="item-details">
-              <h3>{user.title}</h3>
-              <div>
-                <p>{user.notes}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </Fragment>
+    <div>
+      <ul className="users items-list-nav">
+        {users.map((b) => (
+          <li key={b.id} className={b.id === user?.["id"] ? "selected" : null}>
+            <button className="btn" onClick={() => setUser(b)}>
+              {b.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
